639fe8fa3fcceb7504f5da07fd98dbadc65fa1c9
Gergely Szabo <gergely79@gmail.com>
2022-08-22 12:26:36 +0200

    LICENSE added

--- flake.nix
+++
@@ -1,9 +0,0 @@
-{
-
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      devShells.x86_64-linux.default =
-        pkgs.mkShell { packages = [ pkgs.trivy ]; };
-    };
-}
