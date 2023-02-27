84af2c97e83ffd386247facaa5c4c0ee04ed300d
Gergely Szabó <gergely.szabo@origoss.com>
2023-02-27 13:59:17 +0100

    bumping base image (#31)
    from 418.0.0->419.0.0

--- README.md
+++ README.md
@@ -22,12 +22,12 @@ docker push muehlemannpopp/gke-deploy-tools:latest
 
 # Dependency versions
 
-| dependency   | version        | last updated                | digest                                                                  |
-|------------ |-------------- |--------------------------- |----------------------------------------------------------------------- |
-| google-cloud | 418.0.0-alpine | 2023-02-14T21:28:22.381384Z | sha256:2efaf225a4336e94bf0010faa5719981cdef722954271ba9eb8e3497f6100cfb |
-| kustomize    | 5.0.0          | 2023-02-02T17:19:31Z        |                                                                         |
-| sops         | 3.7.3          | 2022-05-09T17:37:50Z        |                                                                         |
-| Helm         | 3.11.1         | 2023-02-08T18:02:00Z        |                                                                         |
+| dependency   | version        | last updated               | digest                                                                  |
+|------------ |-------------- |-------------------------- |----------------------------------------------------------------------- |
+| google-cloud | 419.0.0-alpine | 2023-02-22T16:13:06.29934Z | sha256:e8c6e1e5831b850b33ae5454763300df0617bc8c758b7fcbb979e9e1d6ddb1f8 |
+| kustomize    | 5.0.0          | 2023-02-02T17:19:31Z       |                                                                         |
+| sops         | 3.7.3          | 2022-05-09T17:37:50Z       |                                                                         |
+| Helm         | 3.11.1         | 2023-02-08T18:02:00Z       |                                                                         |
 
 
 ## Google Cloud SDK
--- flake.nix
+++ flake.nix
@@ -1,12 +1,51 @@
 {
   outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      dockerfile-template = pkgs.writeText "dockerfile-template" ''
+        FROM google/cloud-sdk:''${CLOUD_SDK_BASE_IMAGE}@''${CLOUD_SDK_BASE_IMAGE_HASH} AS base
+        ARG KUSTOMIZE_VERSION=''${KUSTOMIZE_VER}
+        ARG SOPS_VERSION=''${SOPS_VER}
+        ARG HELM_VERSION=''${HELM_VER}
+        ADD https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv''${KUSTOMIZE_VERSION}/kustomize_v''${KUSTOMIZE_VERSION}_linux_amd64.tar.gz /tmp
+        ADD https://get.helm.sh/helm-v''${HELM_VERSION}-linux-amd64.tar.gz /tmp
+        RUN tar xf /tmp/kustomize_v''${KUSTOMIZE_VERSION}_linux_amd64.tar.gz -C /usr/bin && \
+            tar xf /tmp/helm-v''${HELM_VERSION}-linux-amd64.tar.gz -C /usr/bin && \
+            wget https://github.com/mozilla/sops/releases/download/v''${SOPS_VERSION}/sops-v''${SOPS_VERSION}.linux.amd64 -O /usr/bin/sops && \
+            chmod a+x /usr/bin/sops
+
+        FROM google/cloud-sdk:''${CLOUD_SDK_BASE_IMAGE}@''${CLOUD_SDK_BASE_IMAGE_HASH}
+        COPY --from=base /usr/bin/kustomize /usr/bin/kustomize
+        COPY --from=base /usr/bin/linux-amd64/helm /usr/bin/helm
+        COPY --from=base /usr/bin/sops /usr/bin/sops
+
+        RUN gcloud components install gke-gcloud-auth-plugin && \
+            gcloud components install kubectl                && \
+            addgroup -S deployer && adduser -S deployer -G deployer
+
+        USER deployer
+        WORKDIR /home/deployer
+      '';
+      dockerfile-generator = pkgs.writeShellApplication {
+        name = "dockerfile-generator";
+        runtimeInputs = [ pkgs.gettext ];
+        text = ''
+          # shellcheck disable=SC2016 
+          envsubst '
+          ''${CLOUD_SDK_BASE_IMAGE}
+          ''${CLOUD_SDK_BASE_IMAGE_HASH}
+          ''${KUSTOMIZE_VER}
+          ''${SOPS_VER}
+          ''${HELM_VER}
+          ' <${dockerfile-template} >Dockerfile 
+        '';
+      };
     in {
       checks.x86_64-linux.dockerfile-linting =
         (pkgs.runCommand "hadolint" { buildInputs = [ pkgs.hadolint ]; } ''
           hadolint ${self}/Dockerfile --ignore DL3047 > $out
         '');
       devShells.x86_64-linux.default =
-        pkgs.mkShell { packages = [ pkgs.trivy ]; };
+        pkgs.mkShell { packages = [ dockerfile-generator ]; };
     };
 }
