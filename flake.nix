79cce3c40522ed632b12f0a2287481603acef040
dockerfile-generator <dockerfile-generator@github.actions>
2024-03-01 20:35:42 +0100

    nushell-based dependency tracking

--- flake.nix
+++ flake.nix
@@ -4,205 +4,18 @@
   outputs = { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      dockerfile-template = pkgs.writeText "dockerfile-template" ''
-        FROM google/cloud-sdk:''${CLOUD_SDK_BASE_IMAGE}@''${CLOUD_SDK_BASE_IMAGE_HASH} AS base
-        ARG KUSTOMIZE_VERSION=''${KUSTOMIZE_VER}
-        ARG SOPS_VERSION=''${SOPS_VER}
-        ARG HELM_VERSION=''${HELM_VER}
-        ADD https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv''${KUSTOMIZE_VERSION}/kustomize_v''${KUSTOMIZE_VERSION}_linux_amd64.tar.gz /tmp
-        ADD https://get.helm.sh/helm-v''${HELM_VERSION}-linux-amd64.tar.gz /tmp
-        RUN tar xf /tmp/kustomize_v''${KUSTOMIZE_VERSION}_linux_amd64.tar.gz -C /usr/bin && \
-            tar xf /tmp/helm-v''${HELM_VERSION}-linux-amd64.tar.gz -C /usr/bin && \
-            wget https://github.com/getsops/sops/releases/download/v''${SOPS_VERSION}/sops-v''${SOPS_VERSION}.linux.amd64 -O /usr/bin/sops && \
-            chmod a+x /usr/bin/sops
 
-        FROM google/cloud-sdk:''${CLOUD_SDK_BASE_IMAGE}@''${CLOUD_SDK_BASE_IMAGE_HASH}
-        COPY --from=base /usr/bin/kustomize /usr/bin/kustomize
-        COPY --from=base /usr/bin/linux-amd64/helm /usr/bin/helm
-        COPY --from=base /usr/bin/sops /usr/bin/sops
-
-        RUN gcloud components install gke-gcloud-auth-plugin && \
-            gcloud components install kubectl                && \
-            addgroup -S deployer && adduser -S deployer -G deployer
-
-        USER deployer
-        WORKDIR /home/deployer
-      '';
-      readme-template = pkgs.writeText "readme-template" ''
-        # Build image
-
-        You can manually build the Docker image with the following command:
-
-        ```bash
-        docker buildx build . -t muehlemannpopp/gke-deploy-tools:latest \
-               --build-arg KUSTOMIZE_VERSION=''${KUSTOMIZE_VER} \
-               --build-arg SOPS_VERSION=''${SOPS_VER} \
-               --build-arg HELM_VERSION=''${HELM_VER}
-        ```
-
-        # Push image
-
-        ```bash
-        docker tag muehlemannpopp/gke-deploy-tools:latest \
-               muehlemannpopp/gke-deploy-tools:''${TOOL_VER}
-        docker push muehlemannpopp/gke-deploy-tools:''${TOOL_VER}
-        docker push muehlemannpopp/gke-deploy-tools:latest
-        ```
-
-
-        # Dependency versions
-
-        | dependency   | version        | last updated               | digest                                                                  |
-        |------------ |-------------- |-------------------------- |----------------------------------------------------------------------- |
-        | google-cloud | ''${CLOUD_SDK_BASE_IMAGE} | ''${CLOUD_SDK_BASE_IMAGE_DATE} | ''${CLOUD_SDK_BASE_IMAGE_HASH} |
-        | kustomize    | ''${KUSTOMIZE_VER} | ''${KUSTOMIZE_DATE} |                                                                         |
-        | sops         | ''${SOPS_VER}          | ''${SOPS_DATE}       |                                                                         |
-        | Helm         | ''${HELM_VER}         | ''${HELM_DATE}       |                                                                         |
-
-
-        ## Google Cloud SDK
-
-        The latest base image version can be checked here:
-
-        <https://hub.docker.com/r/google/cloud-sdk/tags>
-
-
-        ## Kustomize
-
-        The latest `kustomize` version can be checked here:
-
-        <https://github.com/kubernetes-sigs/kustomize/releases>
-
-
-        ## Sops
-
-        The latest `sops` version can be checked here:
-
-        <https://github.com/getsops/sops/releases>
-
-
-        ## Helm
-
-        The latest `Helm` version can be checked here:
-
-        <https://github.com/helm/helm/releases>
-      '';
-
-      dockerfile-generator = pkgs.writeShellApplication {
-        name = "dockerfile-generator";
-        runtimeInputs = [ pkgs.gettext pkgs.jq get-cloud-sdk-script ]
-          ++ get-release-scripts;
+      update-dependencies = pkgs.writeShellApplication {
+        name = "update-dependencies";
+        runtimeInputs = with pkgs; [ gh gettext nushell ];
         text = ''
-
-          cloud_sdk=$(get-cloud-sdk-release)
-          CLOUD_SDK_BASE_IMAGE=$(jq -r '.name' <<<"$cloud_sdk")
-          export CLOUD_SDK_BASE_IMAGE
-          CLOUD_SDK_BASE_IMAGE_DATE=$(jq -r '.published_at' <<<"$cloud_sdk")
-          export CLOUD_SDK_BASE_IMAGE_DATE
-          CLOUD_SDK_BASE_IMAGE_HASH=$(jq -r '.digest' <<<"$cloud_sdk")
-          export CLOUD_SDK_BASE_IMAGE_HASH
-
-          #TOOL_VER=$(sed '$CLOUD_SDK_BASE_IMAGE/-alpine//g'")
-          TOOL_VER="''${CLOUD_SDK_BASE_IMAGE//-alpine/}"
-          export TOOL_VER
-
-          kustomize=$(get-kustomize-release)
-          KUSTOMIZE_VER=$(jq -r '.name' <<<"$kustomize" )
-          export KUSTOMIZE_VER
-          KUSTOMIZE_DATE=$(jq -r '.published_at' <<<"$kustomize")
-          export KUSTOMIZE_DATE
-
-          sops=$(get-sops-release)
-          SOPS_VER=$(jq -r '.name' <<<"$sops" )
-          export SOPS_VER
-          SOPS_DATE=$(jq -r '.published_at' <<<"$sops" )
-          export SOPS_DATE
-
-          helm=$(get-helm-release)
-          HELM_VER=$(jq -r '.name' <<<"$helm" )
-          export HELM_VER
-          HELM_DATE=$(jq -r '.published_at' <<<"$helm" )
-          export HELM_DATE
-
-          echo "TOOL_VER=''${TOOL_VER}"
-          echo "CLOUD_SDK_BASE_IMAGE=''${CLOUD_SDK_BASE_IMAGE}"
-          echo "CLOUD_SDK_BASE_IMAGE_DATE=''${CLOUD_SDK_BASE_IMAGE_DATE}"
-          echo "CLOUD_SDK_BASE_IMAGE_HASH=''${CLOUD_SDK_BASE_IMAGE_HASH}"
-          echo "KUSTOMIZE_VER=''${KUSTOMIZE_VER}"
-          echo "KUSTOMIZE_DATE=''${KUSTOMIZE_DATE}"
-          echo "SOPS_VER=''${SOPS_VER}"
-          echo "SOPS_DATE=''${SOPS_DATE}"
-          echo "HELM_VER=''${HELM_VER}"
-          echo "HELM_DATE=''${HELM_DATE}"
-
-          # shellcheck disable=SC2016
-          envsubst '
-          ''${CLOUD_SDK_BASE_IMAGE}
-          ''${CLOUD_SDK_BASE_IMAGE_HASH}
-          ''${KUSTOMIZE_VER}
-          ''${SOPS_VER}
-          ''${HELM_VER}
-          ' <${dockerfile-template} >Dockerfile
-
-          # shellcheck disable=SC2016
-          envsubst '
-          ''${CLOUD_SDK_BASE_IMAGE}
-          ''${CLOUD_SDK_BASE_IMAGE_HASH}
-          ''${CLOUD_SDK_BASE_IMAGE_DATE}
-          ''${TOOL_VER}
-          ''${KUSTOMIZE_VER}
-          ''${KUSTOMIZE_DATE}
-          ''${SOPS_VER}
-          ''${SOPS_DATE}
-          ''${HELM_VER}
-          ''${HELM_DATE}
-          ' <${readme-template} >README.md
+          nu -n -c 'use ${./lib.nu} *; update-dependencies'
         '';
       };
-
-      get-release-script = name: repo: filter-regex: transform-regex:
-        pkgs.writeShellApplication {
-          name = "get-${name}-release";
-          runtimeInputs = [ pkgs.gh ];
-          text = ''
-            gh api repos/${repo}/releases --jq '. | map(select(.name | . != null and test("${filter-regex}"))) | {name: .[0].name | match ("${transform-regex}").captures[0].string, published_at: .[0].published_at}';
-          '';
-        };
-
-      get-cloud-sdk-script = pkgs.writeShellApplication {
-        name = "get-cloud-sdk-release";
-        runtimeInputs = with pkgs; [ curl jq ];
-        text = ''
-          curl -s https://registry.hub.docker.com/v2/repositories/google/cloud-sdk/tags |
-            jq -r '.results | map(select(.name | . != null and test(".*-alpine$"))) | .[0] | { name: .name, published_at: .tag_last_pushed, digest: .digest }';
-        '';
-      };
-      get-release-scripts-data = {
-        helm = {
-          repo = "helm/helm";
-          filter-regex = "^Helm";
-          transform-regex = "^Helm v(.*)";
-        };
-        kustomize = {
-          repo = "kubernetes-sigs/kustomize";
-          filter-regex = "^kustomize";
-          transform-regex = "^kustomize/v(.*)";
-        };
-        sops = {
-          repo = "getsops/sops";
-          filter-regex = "^v[0-9]+\\\\.[0-9]+\\\\.[0-9]+$";
-          transform-regex = "^v(.*)";
-        };
-      };
-
-      get-release-scripts = builtins.map (name:
-        let value = builtins.getAttr name get-release-scripts-data;
-        in get-release-script name value.repo value.filter-regex
-        value.transform-regex) (builtins.attrNames get-release-scripts-data);
 
       git-create-pr = pkgs.writeShellApplication {
         name = "git-create-pr";
-        runtimeInputs = with pkgs; [ git gh dockerfile-generator ];
+        runtimeInputs = with pkgs; [ git gh update-dependencies ];
         text = ''
           current_branch_name=$(git branch --show-current)
           branch_name=$(date -u +%Y_%m_%d_%H_%M_%S)
@@ -211,7 +24,7 @@
           git checkout -b "''${branch_name}"
 
           echo "Generating Dockerfile and README"
-          dockerfile-generator
+          update-dependencies
 
           echo "Diffing the changes"
           if git diff --exit-code; then
@@ -286,12 +99,13 @@
       devShells.x86_64-linux = {
         default = pkgs.mkShell {
           packages = [
-            dockerfile-generator
+            pkgs.nushell
+            pkgs.gh
+            pkgs.gettext
             git-create-pr
-            get-cloud-sdk-script
             new-release
             nix-update
-          ] ++ get-release-scripts;
+          ];
         };
         git-create-pr = pkgs.mkShell { packages = [ git-create-pr ]; };
         new-release = pkgs.mkShell { packages = [ new-release ]; };
---
+++ lib.nu
@@ -0,0 +1,106 @@
+const github_releases = {
+  helm: {
+    repo: 'helm/helm'
+    filter_regex: '^Helm'
+    transform_regex: '^Helm v?(?<version>[0-9\.]+)$'
+  }
+  kustomize: {
+    repo: 'kubernetes-sigs/kustomize'
+    filter_regex: '^kustomize'
+    transform_regex: '^kustomize/v(?<version>.*)'
+  }
+  sops: {
+    repo: 'getsops/sops'
+    filter_regex: '^v[0-9]+\.[0-9]+\.[0-9]+'
+    transform_regex: '^v(?<version>.*)'
+  }
+}
+
+def release-names () {
+  $github_releases | columns
+}
+
+def "github-release" (release_name: string@release-names) {
+  let release = $github_releases | get $release_name
+  let latest_release = ^gh api $'repos/($release.repo)/releases' | from json | where name =~ ($release.filter_regex) | get 0
+  {
+    version: ($latest_release.name | parse --regex $release.transform_regex | get version?.0? )
+    published_at: $latest_release.published_at
+  }
+}
+
+def "cloud-sdk-release" () {
+  let latest_release = http get 'https://registry.hub.docker.com/v2/repositories/google/cloud-sdk/tags' | get results | where name =~ '.*-alpine$' | get 0
+  {
+    image_tag: $latest_release.name
+    published_at: $latest_release.tag_last_pushed
+    version: ($latest_release.name | parse --regex '^(?<version>[0-9\.]+)-alpine$' | get version?.0?)
+    digest: $latest_release.digest
+  }
+}
+
+def "collect-versions" () {
+  release-names | reduce --fold {} { |it, acc| $acc | upsert $it (github-release $it)} | upsert 'cloud-sdk' (cloud-sdk-release)
+}
+
+def "version-update-needed?" (new_version) {
+  let old_version = open 'versions.nuon'
+  # release-names | append "cloud-sdk" | any { |rel_name|
+  #   ($new_version | get $rel_name | get version) != ($old_version | get $rel_name | get version)
+  # }
+  $new_version.cloud-sdk.version != $old_version.cloud-sdk.version
+}
+
+def "update-version" () {
+  let new_version = collect-versions
+  let update_needed = version-update-needed? $new_version
+  if $update_needed {
+    print 'Updating version'
+    $new_version | save -f 'versions.nuon'
+  } else {
+    print 'No version update is needed'
+  }
+  $update_needed
+}
+
+export def "update-readme" () {
+  let versions = open 'versions.nuon'
+  let envs = {
+    CLOUD_SDK_BASE_IMAGE: $versions.cloud-sdk.image_tag
+    CLOUD_SDK_BASE_IMAGE_HASH: $versions.cloud-sdk.digest
+    CLOUD_SDK_BASE_IMAGE_DATE: $versions.cloud-sdk.published_at
+    TOOL_VER: $versions.cloud-sdk.version
+    KUSTOMIZE_VER: $versions.kustomize.version
+    KUSTOMIZE_DATE: $versions.kustomize.published_at
+    SOPS_VER: $versions.sops.version
+    SOPS_DATE: $versions.sops.published_at
+    HELM_VER: $versions.helm.version
+    HELM_DATE: $versions.helm.published_at
+  }
+  let env_list = $envs | columns | each { |e| '${' + $e + '}' } | str join ' '
+  with-env $envs {
+    open templates/README.md | ^envsubst $env_list | save -f README.md
+  }
+}
+
+def "update-dockerfile" () {
+  let versions = open 'versions.nuon'
+  let envs = {
+    CLOUD_SDK_BASE_IMAGE: $versions.cloud-sdk.image_tag
+    CLOUD_SDK_BASE_IMAGE_HASH: $versions.cloud-sdk.digest
+    KUSTOMIZE_VER: $versions.kustomize.version
+    SOPS_VER: $versions.sops.version
+    HELM_VER: $versions.helm.version
+  }
+  let env_list = $envs | columns | each { |e| '${' + $e + '}' } | str join ' '
+  with-env $envs {
+    open templates/Dockerfile | ^envsubst $env_list | save -f Dockerfile
+  }
+}
+
+export def "update-dependencies" () {
+  if (update-version) {
+    update-readme
+    update-dockerfile
+  }
+}
